cd8dd8c2748f84daf50bf2c2de9cc09088ac690d
Emily G <aspentree79@gmail.com>
2019-08-07 19:54:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_second_tier
 				:reading_railroad => {}
 				:pennsylvania_railroad => {}
 				:b_and_o_railroad => {}
-				:shortline_railroad => {} 
+				:shortline_railroad => {}
 				}
 		 }
 end
